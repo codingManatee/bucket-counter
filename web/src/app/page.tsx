4e722830c2212e1f6893d5ce6b366eaeadf88c07
WorkingDada <6430222521@student.chula.ac.th>
2025-04-02 00:51:11 +0700

    feat : add web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import BucketLoggerDemo from "@/components/bucket-logger";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen rounded-lg  overflow-hidden ">
+      <BucketLoggerDemo />
+    </main>
+  );
+}
